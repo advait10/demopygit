42a160ce6b84def5eaab553ffdaaf886e5cf590e
Python8007 <rakeshsawant47@gmail.com>
2019-12-25 19:18:24 +0530

    New commit  added

--- demo.py
+++ demo.py
@@ -1,2 +1,22 @@
 print('Hello..')
 print('welcome to telusko..')
+
+
+#----------New Commit---------------
+'''
+1   5
+ 2 4
+  3
+'''
+n=3
+spaces = 0
+import time
+# def func(n):
+#     global spaces
+for i in range(n):
+    # spaces=
+    if i==(n-1):
+        print('-' * (i+1),'*')
+    else:
+        print('-'*i,'*','-'*(2*n-(2*i+3)),'*')
+    # time.sleep(3)
